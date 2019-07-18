e69740d1113fb50fb84f44f338213a6573f31c60
Rob Wood <robwood@homemail.com>
2019-07-18 12:33:01 -0700

    Corrected megnetism and velocity iteration issue

--- particles.py
+++ particles.py
@@ -21,51 +21,47 @@ class Particle(object):
         self.previousPos=previousPos
         self.currentPos=previousPos
 
-        self.static=static   #currently unimplemented
+        self.static=static
 
-    def d(self,p2):
+    def r_vec(self,p2):
         return np.subtract(self.currentPos,p2.currentPos)
 
     def r(self,p2):
-        return np.linalg.norm(self.d(p2))
+        return np.linalg.norm(self.r_vec(p2))
 
     def r_hat(self,p2):
-        return np.divide(self.d(p2),self.r(p2))
+        return np.divide(self.r_vec(p2),self.r(p2))
 
     def reduceMasses(self,p2):
         self.reducedMass=p2.reducedMass=\
                 np.divide(self.mass*p2.mass,self.mass+p2.mass)
+        return self.reducedMass
 
     def coulomb(self,p2):
-        top=(self.K*self.charge*p2.charge*(
-            self.r_hat(p2)
-            /self.r(p2))
-            )
+        top=(self.K*self.charge*p2.charge*self.r_hat(p2))
         bottom=np.power(self.r(p2),2)
 
-        self.force=top/bottom
+        return top/bottom
 
     def magnetism(self,p2):
-        top=(sci.mu_0*self.charge*np.cross(
-            self.currentVel,
-            self.r_hat(p2),
-            axis=1
+        top=(sci.mu_0*p2.charge*np.cross(
+            p2.currentVel,
+            self.r_hat(p2)
             ))
         bottom=(4*sci.pi*np.power(self.r(p2),2))
         B=top/bottom
 
-        F=(p2.charge*np.cross(
-            p2.currentVel,
-            B,
-            axis=1
+        F=(self.charge*np.cross(
+            self.currentVel,
+            B
             ))
 
-        p2.force=np.add(p2.force,F)
+        return F
 
     def accumulateForces(self,p2):
-        self.coulomb(p2)
-        self.magnetism(p2)
+        self.force=self.coulomb(p2)+self.magnetism(p2)
         return self.force
+        #POSSIBLE ISSUE: force of magnetism seems negligible ???
 
     def updateVelocity(self,t):
         if not self.static:
@@ -90,9 +86,11 @@ class Simulation(object):
         self.time=time
         self.steps=time*1000
         self.timerange=np.linspace(0,time,steps)
+        self.timestep=self.timerange[1]
 
     def simulate(self):
         self.p1.reduceMasses(self.p2)
+        self.p2.reduceMasses(self.p1)
 
         outfile=open('out.txt','w')
 
@@ -100,16 +98,16 @@ class Simulation(object):
             self.p1.accumulateForces(self.p2)
             self.p2.accumulateForces(self.p1)
 
-            self.p1.updateVelocity(i)
-            self.p2.updateVelocity(i)
+            self.p1.updateVelocity(self.timestep)
+            self.p2.updateVelocity(self.timestep)
 
-            self.p1.updatePosition(i)
+            self.p1.updatePosition(self.timestep)
             outfile.write(
                     str(i)+" "+
                     str(self.p1.currentPos[0,0])+" "+
                     str(self.p1.currentPos[0,1])+" "+
                     str(self.p1.currentPos[0,2])+"\n")
-            self.p2.updatePosition(i)
+            self.p2.updatePosition(self.timestep)
             outfile.write(
                     str(i)+" "+
                     str(self.p2.currentPos[0,0])+" "+
@@ -121,26 +119,23 @@ class Simulation(object):
 
 def main():
     #Initial velocity np arrays
-    v1=np.array([[0,0,0]])
-    v2=np.array([[0.1,0.1,0.1]])
+    v1 = np.array([[1.0,1.0,1.0]])
+    v2 = np.array([[-1.,-1.,-1.]])
 
     #Initial position np arrays
-    p1=np.array([[0.0,0.0,0.0]])
-    p2=np.array([[0.1,0.1,0.1]])
-
+    p1 = np.array([[0.07234652,0.09779895,0.05384959]])
+    p2 = np.array([[0.09992369,0.09922008,0.09956159]])
+    
     proton=Particle(sci.e,sci.m_p,v1,p1)
     electron=Particle(-sci.e,sci.m_e,v2,p2)
     
-    t=1
+    t=.005
 
-    sim=Simulation(proton,electron,t)
+    sim=Simulation(proton,electron,t,1000)
     sim.simulate()
 
-    print("Reduced mass of system: ",proton.reducedMass, electron.reducedMass)
-
-    print("After",sim.steps,"iterations over",t,"seconds:")
-    print("Final position of proton   ",proton.currentPos)
-    print("Final position of electron ",electron.currentPos)
+    print("pos_f, proton\n",proton.currentPos)
+    print("pos_f, electron\n",electron.currentPos)
 
 if __name__ == '__main__':
     main()
